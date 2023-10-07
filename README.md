2c849890e6c4184ab06921700cb77f9fa5b11f34
flskhhdf <flskhhdf@naver.com>
2023-10-08 01:30:25 +0900

    cookbook

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # React_CookBook
+
+설치된 모듈 
+폰트어썸{
+npm i @fortawesome/fontawesome-svg-core
+npm i @fortawesome/free-solid-svg-icons @fortawesome/free-regular-svg-icons @fortawesome/free-brands-svg-icons
+npm i @fortawesome/react-fontawesome
+}
--- cookbook/src/App.js
+++ cookbook/src/App.js
@@ -1,10 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { faUtensils } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
     <div className="App">
-
+      <nav className='nav_Bar'>
+        <ul>
+          <li> <a href='#'> <FontAwesomeIcon icon={faUtensils} size='1x'/> 쿠크부크 <br/>Cook Book</a> </li>
+          <li> <a href='#'>레시피</a> </li>
+          <li> <a href='#'>커뮤니티</a> </li>
+          <li> <a href='#'>Search Bar</a> </li>
+          <li> <a href='#'>공지사항</a> </li>
+          <li className='search_Bar'>
+            <input type='text' size='1'></input> 
+            <FontAwesomeIcon icon={faMagnifyingGlass} size='1x' className='fa-search'/>
+          </li>
+        </ul>
+      </nav>
+      <div className='container'>
+        <div className='main-content'>
+          <div className='main-today_recipe recipe'>
+            <p>오늘의 새 레시피</p>
+          </div>
+          <div className='main-recommend_recipe recipe'>
+            <p>오늘의 추천 레시피</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
